81c2acc8469c369237aa4c351915c86b69177644
theazarov <80588590+theazarov@users.noreply.github.com>
2022-01-08 18:48:39 +0300

    hi

--- higher_textbook_23-24/main.js
+++ higher_textbook_23-24/main.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
 //Карманы в самой регулярке в JavaScript:
 
-let str = 'aa bb cd ef';
-let result =  str.replace(/([a-z])\1/g, '!')
-console.log(result)
+let str = "aa bb cd ef";
+let result = str.replace(/([a-z])\1/g, "!");
+console.log(result);
 
-str = 'asxca buzxb csgd';
-result = str.replace(/([a-z])[a-z]+\1/g, '!')
-console.log(result)
+str = "asxca buzxb csgd";
+result = str.replace(/([a-z])[a-z]+\1/g, "!");
+console.log(result);
 
 /*Задача 1js.Sp.RE.EP
 
@@ -17,9 +17,9 @@ console.log(result)
 let str = 'aaa bbb ccc xyz';
 Найдите все подстроки, в которых есть три одинаковые буквы подряд.*/
 
-str = 'aaa bbb ccc xyz';
-result = str.replace(/([a-z])\1\1\1/g, '!')
-console.log(result)
+str = "aaa bbb ccc xyz";
+result = str.replace(/([a-z])\1\1\1/g, "!");
+console.log(result);
 
 /*Задача 3js.Sp.RE.EP
 
@@ -28,19 +28,19 @@ console.log(result)
 let str = 'aaa aaa bbb bbb ccc ddd';
 Найдите все подстроки, в которых есть два одинаковых слова подряд.*/
 
-str = 'aaa aaa bbb bbb ccc ddd';
-result = str.replace(/([a-z]+)\s\1/g, '!')
-console.log(result)
+str = "aaa aaa bbb bbb ccc ddd";
+result = str.replace(/([a-z]+)\s\1/g, "!");
+console.log(result);
 
 //Несохраняющие скобки в регулярках JavaScript:
 
-str = 'abab123';
+str = "abab123";
 result = str.match(/(?:ab)+([1-9]+)/);
-console.log(result[1])
+console.log(result[1]);
 
-str = '2021/11/08'
-result = /(^[^0]\d{1,4})+\/+(0|1)+([0-9])+\/([0-3])+([0-9])/.test(str)
-console.log(result)
+str = "2021/11/08";
+result = /(^[^0]\d{1,4})+\/+(0|1)+([0-9])+\/([0-3])+([0-9])/.test(str);
+console.log(result);
 
-result = str.replace(/(^[^0]\d{1,4})+\/+(0|1)+([0-9])+\/([0-3])+([0-9])/g, '!')
-console.log(result)
+result = str.replace(/(^[^0]\d{1,4})+\/+(0|1)+([0-9])+\/([0-3])+([0-9])/g, "!");
+console.log(result);
---
+++ higher_textbook_25-26/main.js
@@ -0,0 +1,89 @@
+'use strict';
+
+//Позитивный и негативный просмотр в JavaScript:
+
+console.log('aaax aaab'.replace(/aaa(?=x)/g, '!'))
+console.log('aaax aaab'.replace(/aaa(?!x)/g, '!'))
+
+/*Задача 1js.Sp.RE.PNV
+
+Дана строка, содержащая имена функций:
+
+let str = 'func1() func2() func3()';
+Получите массив имен функций из строки.*/
+
+let str = 'func1() func2() func3()';
+let result = str.match(/func\d{1,}/g, '!')
+console.log(result)
+
+/*Задача 2js.Sp.RE.PNV
+
+Дана строка с тегом:
+
+let str = '<a href="" class="eee" id="zzz">';
+Получите массив имен атрибутов этого тега.*/
+
+str = '<a href="" class="eee" id="zzz">';
+result = str.match(/([a-z])\1\1/g, '!')
+console.log(result)
+
+//Коллбэк в методе replace в регулярках JavaScript:
+
+str = '2 u u03 aaa 45';
+result = str.replace(/\d+/g, function(num){
+    return num**2
+})
+console.log(result)
+
+str = '2+3= 4+5= 6+7=';
+result = str.replace(/(\d+)\+(\d+)=/g, function(match, num1, num2){
+    let sum = Number(num1) + Number(num2)
+    return match + sum
+})
+console.log(result)
+
+/*Задача 1js.Sp.RE.RMC
+
+Дана строка:
+
+let str = 'aaa [2] bbb [3] ccc [12] ddd';
+Найдите числа, стоящие в скобках и увеличьте их в два раза. То есть из нашей строки должна получится следующая:
+
+'aaa [6] bbb [9] ccc [24] ddd'*/
+
+str = 'aaa [2] bbb [3] ccc [12] ddd';
+result = str.replace(/(\d+)/g, function(match, num){
+    let sum = Number(num) + Number(num)
+    return sum
+})
+console.log(result)
+
+/*Задача 2js.Sp.RE.RMC
+
+Дана строка:
+
+let str = '123 456 789';
+Найдите все числа и переверните их цифры в обратном порядке. То есть из нашей строки должна получится следующая:
+
+'321 654 789'*/
+
+str = '123 456 789';
+result = str.replace(/(\d)+(\d)+(\d)/g, function(match, one, two, three){
+    return three + two + one
+})
+console.log(result)
+
+/*Задача 3js.Sp.RE.RMC
+
+Дана строка с датами:
+
+let str = '31.12.2025 30.11.2024 29.10.2023';
+Найдите все даты и преобразуйте их в другой формат так, чтобы получилась следующая строка:
+
+'2025-12-31 2024-11-30 2023-10-29'*/
+
+str = '31.12.2025 30.11.2024 29.10.2023';
+result = str.replace(/(\d+)\.(\d+)\.(\d+)/g, function(match, day, month, year){
+    return year + '-' + month + '-' + day
+})
+console.log(result)
